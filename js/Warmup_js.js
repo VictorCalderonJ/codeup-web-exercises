1a1e6bc4bad69db4cc2482ec830ec51c6d79a3a7
victor calderon <calderon.j.victor@gmail.com>
2023-06-07 10:58:26 -0500

    0607

--- js/Warmup_js.js
+++ js/Warmup_js.js
@@ -27,22 +27,78 @@
 // console.log(addTen(10))
 // console.log(addTen('bob'))
 
-function makeUpperCase(x) {
+// function makeUpperCase(x) {
+//
+//
+//     if (isNaN(x)) {
+//         return x.toUpperCase()
+//     } else {
+//         return false
+//     }
+// }
+//
+// console.log(makeUpperCase('cat')).log; // returns 'CAT'
+// console.log(makeUpperCase('DoG')); // returns 'DOG'
+// console.log(makeUpperCase('CODEUP')); // returns 'CODEUP'
+// console.log(makeUpperCase(54)); // returns false
+// console.log(makeUpperCase(true)); // returns false
+// console.log(makeUpperCase(null)); // returns false
+// console.log(makeUpperCase('5'))
+// console.log(makeUpperCase("5dogs"))
+
+// let x = 1
+// while (x <= 10) {
+//     console.log("Victor")
+//     x++
+// }
+
+/*
+        TODO - write a do-while loop that will alert the current date and time and
+          continue checking with the user if they want to see the latest time.
+
+          Current date can be created with new Date().toLocaleString()
+     */
 
 
-    if (isNaN(x)) {
-        return x.toUpperCase()
+
+// do {
+//     alert(`The current date and local time is: \n\n\ ${new Date().toLocaleString()}`)
+// } while((confirm(`Would the user want to know the date and time?`)))
+
+
+// ++i or i += 1 (increase by the number you have listed
+
+/*
+    TODO - write a for loop that prints all even numbers from 2 to 100
+
+    TODO - write a for loop that prints all integers from 20 down to 1
+ */
+
+
+// for (let x = 2; x <= 100; x += 2, ) {
+//     console.log(`Value of x: ${x}`);
+//}
+
+// for (let y = 20, y >= 1, --y {
+//      console.log(`Value of y: ${y}`);
+//}
+
+function greet(name) {
+
+    if (name === "Johnny") {
+        return `Hello, my love!`;
     } else {
-        return false
+        return `Hello, ${name}!`
     }
+
 }
-
-console.log(makeUpperCase('cat')).log; // returns 'CAT'
-console.log(makeUpperCase('DoG')); // returns 'DOG'
-console.log(makeUpperCase('CODEUP')); // returns 'CODEUP'
-console.log(makeUpperCase(54)); // returns false
-console.log(makeUpperCase(true)); // returns false
-console.log(makeUpperCase(null)); // returns false
-console.log(makeUpperCase('5'))
-console.log(makeUpperCase("5dogs"))
-
+console.log(greet('victor'))
+console.log(greet('calderon'))
+console.log(greet('Johnny'))
+console.log(greet('Sam'))
+// function greet(name){
+//     return "Hello, " + name + "!";
+//
+//     if(name === "Johnny") {
+//         return "Hello, my love!";
+//     }
